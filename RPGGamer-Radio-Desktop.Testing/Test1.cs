ae3573cf48eda2af77bc3d1b036edd54d7eb03ba
Derek Gooding <djgooding@live.com>
2025-01-01 19:11:22 -0500

    Add unit testing

---
+++ RPGGamer-Radio-Desktop.Testing/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace RPGGamer_Radio_Desktop.Testing
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
