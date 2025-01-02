123d28cfab324c6b0c27fdcfc0bf2cc1dbb0d952
Derek Gooding <djgooding@live.com>
2025-01-02 17:59:21 -0500

    Add publishing and testing

---
+++ RPGGamer-Radio-Desktop.Text/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace RPGGamer_Radio_Desktop.Text
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
